eddb845a22d68ecc08f1e8d76c897212f669fce0
pitashpalmal <pitash.it@palmalgarments.com>
2022-08-07 16:33:17 +0600

    Components And Props

---
+++ code/1. element and render/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+setInterval(()=> {
+  const element = (
+    <h1 className="heading">
+      <span className="text">Hello {new Date().toLocaleTimeString()}</span>
+    </h1>
+  );
+
+  ReactDOM.render(element, document.getElementById('root'));
+},1000);
