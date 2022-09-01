45998fe194c34c669bd6156c2695db6d6dd1b8bb
pitashpalmal <pitash.it@palmalgarments.com>
2022-09-01 19:18:38 +0600

    Form Handling - Controlled vs Uncontrolled

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import './App.css';
-import ClockList from './components/ClockList';
+import Form from './components/Form';
 
 function App() {
-  const quantities = [1,2,3];
   return (
     <div>
-      {/* <Clock></Clock> */}
-      <ClockList quantities ={quantities} />
+      <Form />
     </div>
   );
 }
