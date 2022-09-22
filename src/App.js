9d59ae41509d152a3120b541606f9381d2532ca4
pitashpalmal <pitash.it@palmalgarments.com>
2022-09-22 12:19:42 +0600

    Composition vs Inheritance

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import Text from "./components/inheritance/Text";
+import Emoji from "./components/composition/Emoji";
+import Text from "./components/composition/Text";
+import Bracket from "./components/composition/Bracket";
+
+// function App() {
+//   return <Emoji><Text /></Emoji>;
+//   // here Emoji is parent and Text component is a props.children
+// }
 
 function App() {
-  return <Text />;
+    return(
+      <Emoji>
+          { ({addEmoji}) => (
+            <Bracket>
+                {({addBracket}) => <Text addEmoji={addEmoji} addBracket={addBracket} />}
+            </Bracket>
+          ) }
+      </Emoji>
+    );
 }
 
 export default App;
